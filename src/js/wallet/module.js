69e5c1a7237518a1c4fb44401f9ddc309b38cb41
Michaël van de Giessen <git@tubby.nl>
2018-03-14 17:02:03 +0100

    functional attempt of having a katalyst-waves pair in the dex

--- src/js/wallet/module.js
+++ src/js/wallet/module.js
@@ -7,12 +7,5 @@
             WALLET_WITHDRAW: 'wallet-withdraw',
             WALLET_DEPOSIT: 'wallet-deposit',
             WALLET_CARD_DEPOSIT: 'wallet-card-deposit'
-        })
-        .constant('currency.KAT', Currency.create({
-            id: 'DSakmDaduNHXgAXKP4LM7358yZYduWg9VGALA9auDck8',
-            displayName: 'Katalyst',
-            shortName: 'KAT',
-            precision: 8,
-            verified: false
-        }));
+        });
 })();
